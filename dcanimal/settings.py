16f8fec41d3f6c5c6b8e9d15b4754b2a24ca894a
NisargShah912 <112761621+NisargShah912@users.noreply.github.com>
2022-12-27 17:43:51 +0530

    Add files via upload

--- dcanimal/settings.py
+++ dcanimal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(mph1st+z9iz_ph-!)^_n88_f6yb7hwwef#moy@i%$5&%d9pp8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- dcanimal/urls.py
+++ dcanimal/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from dcan import views
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    path('', include('dcan.urls')),
+    path('', views.home, name='home'),
+    path('regan', views.qr_gen, name='qrgen'),
+    path('login', views.login, name="login"),
+    path('checklogin', views.checklogin, name="checklogin"),
+    path('adduser', views.adduser, name="user"),
+    path('signup', views.signup, name="signup"),
+    path('contact', views.contact, name="contact"),
+    path('graph', views.graph, name="graph"),
     
 ]
 
